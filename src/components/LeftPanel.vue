812c5d1d04473ab0ea2ee61a03acd2f7e4c3ee04
Maka5000 <mak.khas01@gmail.com>
2023-07-05 23:39:36 +0600

    feat: style LeftPanel

--- src/components/LeftPanel.vue
+++ src/components/LeftPanel.vue
@@ -1,18 +1,24 @@
 <template>
     <ul>
+        <li><h1 class="logo">CV</h1></li>
         <li>
             <router-link to="/">
-                <button>Home</button>
+                <button class="home-btn"></button>
             </router-link>
         </li>
         <li>
             <router-link to="/about">
-                <button>About</button>
+                <button class="about-btn"></button>
             </router-link>
         </li>
         <li>
             <router-link to="/projects">
-                <button>Projects</button>
+                <button class="projects-btn"></button>
+            </router-link>
+        </li>
+        <li>
+            <router-link to="/download">
+                <button class="download-btn"></button>
             </router-link>
         </li>
     </ul>
@@ -20,11 +26,44 @@
 
 <style scoped>
     ul {
+        margin: 30px 0;
         list-style-type: none;
         background-color: blue;
+        display: grid;
+        grid-template-rows: 2fr 1fr 1fr 1fr 2fr;
     }
 
-    li {
-        margin: 5px 0;
+    .logo {
+        margin-top: 25px;
+        text-align: center;
+    }
+
+    button {
+        width: 100%;
+        height: 50px;
+        background-color: transparent;
+        background-repeat: no-repeat;
+        background-position: center;
+        outline: none;
+        border: none;
+        filter: invert(100%);
+    }
+
+    .home-btn {
+        background-image: url("../assets/Icons/LeftPanel/home.svg");
+    }
+
+    .about-btn {
+        background-image: url("../assets/Icons/LeftPanel/aboutme.svg");
+    }
+
+    .projects-btn {
+        background-image: url("../assets/Icons/LeftPanel/projects.svg");
+    }
+
+    .download-btn {
+        background-image: url("../assets/Icons/LeftPanel/download.svg");
+        background-size: contain;
+        margin-top: 100%;
     }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,3 +4,11 @@
         <p>Lorem ipsum dolor sit amet.</p>
     </div>
 </template>
+
+<style scoped>
+    div {
+        padding-top: 25px;
+        margin: 30px 0;
+        background-color: blue;
+    }
+</style>
