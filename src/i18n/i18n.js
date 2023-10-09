cefae51e7732767797b3e0141c4876f670099424
Maka5000 <mak.khas01@gmail.com>
2023-10-09 15:59:48 +0600

    fix: translate en, rus add Courses

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -2,7 +2,7 @@ import en from "./en.json";
 import rus from "./rus.json";
 import kz from "./kz.json"
 
-export const defaultLocale = "rus";
+export const defaultLocale = "en";
 
 export const languages = {
     en,
